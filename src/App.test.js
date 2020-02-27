e6b495791e7aaeff52c7f20477f07f6d19d15c30
Ollie Holden <ollie@Nology.local>
2020-02-27 09:15:08 +0000

    changed the tests to align with new homepage

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the page correctly', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/blank page deployment/i);
+  const linkElement = getByText(/The Drop x Nology/i);
   expect(linkElement).toBeInTheDocument();
 });
