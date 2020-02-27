6c9cd351b833dd8bb2402a8fcbec0b526c1c34e6
Andy Cameron <Andy.Cameron@Nology.local>
2020-02-27 17:58:15 +0000

    Created first iteration of navbar. Separated _variables as partial and added logo to images.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.module.scss';
 import LandingPage from '../../components/LandingPage';
+import Navbar from '../../components/Navbar';
 
 function App() {
   return (
     <main>
-       <LandingPage />
+       {/* <LandingPage /> */}
+        <Navbar />
     </main>
   );
 }
