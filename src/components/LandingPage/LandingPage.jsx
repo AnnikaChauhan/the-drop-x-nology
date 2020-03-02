a6b064f020f8f28fe95d71dd1e680fb84edf7363
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-02 13:58:29 +0000

    first draft, added pictures, animations, shadows and logo

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,10 +6,12 @@ class LandingPage extends Component {
         return (
             <section className={styles.container}>
                 <div className={styles.left}>
+                    <div className={styles.logoimg}></div>
                     <h2>Artists</h2>
                 </div>
-                    
+                
                 <div className={styles.right}>
+                    <div className={styles.logoimg}></div>
                     <h2>Fans</h2>
                 </div>
             </section>
