bf0220427caeb18880dcb6b2832bb40658d60707
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-02 15:12:26 +0000

    Created a Main container for components. Navbar has an additional overlay to occupy space in the document flow.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import styles from './Navbar.module.scss';
-import logo from '../../static/images/logo-white.png';
+import React, { Component } from "react";
+import styles from "./Navbar.module.scss";
+import logo from "../../static/images/logo-white.png";
 
 class Navbar extends Component {
     render() {
         return (
-            <nav>
-                <img src={logo} />
-                <ul>
-                    <li><a href=''>Dashboard</a></li>
-                    <li><a href=''>User Profile</a></li>
-                    <li><a href=''>Help and Contact</a></li>
-                    <li><a href=''>Legal</a></li>
-                    <li><a href=''>Feedback</a></li>
-                    <li><a href=''>Logout</a></li>
-                </ul>
-            </nav>
+            <div className={styles.Nav}>
+                <nav>
+                    <img src={logo} alt="The Drop" />
+                    <ul>
+                        <li>
+                            <a href="">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="">User Profile</a>
+                        </li>
+                        <li>
+                            <a href="">Help and Contact</a>
+                        </li>
+                        <li>
+                            <a href="">Legal</a>
+                        </li>
+                        <li>
+                            <a href="">Feedback</a>
+                        </li>
+                        <li>
+                            <a href="">Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+                {/* Overlay required for Main to position itself in document flow with a fixed Navbar */}
+                <div className={styles.overlay} />
+            </div>
         );
     }
 }
