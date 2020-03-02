408b3dc6dde42f18c8604eeff76dbf946298553e
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-02 15:41:28 +0000

    added responsive design and removed certain hover feautres, added logo to the top right too

--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -4,9 +4,10 @@ import styles from "./LandingPage.module.scss";
 function LandingPage() {
         return (
             <section className={styles.container}>
+                <div className={styles.mainlogo}></div>
                 <div className={styles.left}>
                     <div className={styles.wrapper}>
-                        <div className={styles.logoimg}></div>
+                        {/* <div className={styles.logoimg}></div> */}
                         <p>I'm an:</p>
                         <h2>Artist</h2>
                     </div>
@@ -14,7 +15,7 @@ function LandingPage() {
                 
                 <div className={styles.right}>
                     <div className={styles.wrapper}>
-                        <div className={styles.logoimg}></div>
+                        {/* <div className={styles.logoimg}></div> */}
                         <p>I'm a:</p>
                         <h2>Fan</h2>
                     </div>
