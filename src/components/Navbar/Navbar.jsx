a93cd2d812d47283d9817e566b4ebc7ad5dc3b5a
Jonny Tse <Jonny.Tse@Nology.local>
2020-03-02 17:36:27 +0000

    created new mobile hamburger navbar pre 768px vs standard desktop navbar post 769px but not responsive unless hard page refresh

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
 import styles from './Navbar.module.scss';
 import logo from '../../static/images/logo-white.png';
+import navStyleData from '../../static/data/navStyleData.js';
+import { slide as Menu } from 'react-burger-menu';
 
 class Navbar extends Component {
-    render() {
-        return (
-            <nav>
-                <img src={logo} />
-                <ul>
-                    <li><a href=''>Dashboard</a></li>
-                    <li><a href=''>User Profile</a></li>
-                    <li><a href=''>Help and Contact</a></li>
-                    <li><a href=''>Legal</a></li>
-                    <li><a href=''>Feedback</a></li>
-                    <li><a href=''>Logout</a></li>
-                </ul>
-            </nav>
-        );
+    showSettings (event) {
+        event.preventDefault();
+      }
+
+    render() {        
+        if (window.innerWidth <= 768) {
+            return (
+                <Menu 
+                    noOverlay 
+                    disableOverlayClick 
+                    disableAutoFocus
+                    styles={navStyleData} 
+                    width={ "100%" }>
+                    <img src={logo} />
+                    <ul>
+                        <li><a id="dashboard" className="menu-item" href=''>Dashboard</a></li>
+                        <li><a id="user" className="menu-item" href=''>User Profile</a></li>
+                        <li><a id="help" className="menu-item" href=''>Help and Contact</a></li>
+                        <li><a id="legal" className="menu-item" href=''>Legal</a></li>
+                        <li><a id="feedback" className="menu-item" href=''>Feedback</a></li>
+                        <li><a id="logout" className="menu-item" href=''>Logout</a></li>
+                        <li><a onClick= { this.showSettings } className="menu-item--small" href=''></a></li>
+                    </ul>
+                </Menu>)
+        } else {
+            return (
+                <nav>
+                    <img src={logo} />
+                    <ul>
+                        <li><a id="dashboard" className="menu-item" href=''>Dashboard</a></li>
+                        <li><a id="user" className="menu-item" href=''>User Profile</a></li>
+                        <li><a id="help" className="menu-item" href=''>Help and Contact</a></li>
+                        <li><a id="legal" className="menu-item" href=''>Legal</a></li>
+                        <li><a id="feedback" className="menu-item" href=''>Feedback</a></li>
+                        <li><a id="logout" className="menu-item" href=''>Logout</a></li>
+                        <li><a onClick= { this.showSettings } className="menu-item--small" href=''></a></li>
+                    </ul>
+                </nav>
+            );
+        }
     }
 }
 
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.module.scss';
 import LandingPage from '../../components/LandingPage';
 import Navbar from '../../components/Navbar';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <main>
-            <LandingPage />
-
+            {/* <LandingPage /> */}
+            <Navbar />
         </main>
     );
 }
