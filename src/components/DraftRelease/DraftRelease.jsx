503aed20ce6630e20efa5316e151f1a744545ffa
Sam Joyce <Sam.Joyce@Nology.local>
2020-03-02 17:37:20 +0000

    draft release page - began adding input forms etc and relevant formatting

--- src/components/DraftRelease/DraftRelease.jsx
+++ src/components/DraftRelease/DraftRelease.jsx
@@ -1,26 +1,62 @@
 import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./DraftRelease.module.scss";
+
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class DraftRelease extends Component {
     render() {
         return (
             <section>
-                <article>
-                    <h1>Manage Release</h1>
+                <article className={styles.topPart}>
+                    <h2>Manage Release</h2>
                     <div>
                         <button>SAVE</button>
                         <button>PREVIEW</button>
                         <button>PUBLISH</button>
                     </div>
                 </article>
-                <article>
+                <article className={styles.initialDescription}>
                     <h4>Type</h4>
-                    <input type="radio" name="type" id="" value="Single"/>
-                    <input type="radio" name="type" id="" value="EP"/>
-                    <input type="radio" name="type" id="" value="Album"/>
-                    <input type="radio" name="type" id="" value="Other"/>
+                    <div>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                select type from below list...
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Single</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">EP</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Album</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <form>
+                        <h4>Title</h4>
+                        <input type="text"/>
+                        <h4>Description</h4>
+                        <input type="text"/>
+                    </form>
                 </article>
+                <div>
+                    <h4>Announcement</h4>
+                    <div>
+                        <input type="text" id="day1" placeholder="D"/>
+                        <input type="text" id="day2" placeholder="D"/>
+                        <input type="text" id="day3" placeholder="M"/>
+                        <input type="text" id="day4" placeholder="M"/>
+                        <input type="text" id="day5" placeholder="Y"/>
+                        <input type="text" id="day6" placeholder="Y"/>
+                        <input type="text" id="day7" placeholder="Y"/>
+                        <input type="text" id="day8" placeholder="Y"/>
+                        <input type="text" id="day9" placeholder="0"/>
+                        <input type="text" id="day10" placeholder="0"/>
+                        <input type="text" id="day11" placeholder="0"/>
+                    </div>
+                </div>
             </section>
-        )
-    }
-}
+                    )
+                }
+            }
+            
+export default DraftRelease;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.module.scss';
 import LandingPage from '../../components/LandingPage';
+import DraftRelease from "../../components/DraftRelease/DraftRelease";
 
 function App() {
   return (
     <main>
-       <LandingPage />
+      <LandingPage />
+      <DraftRelease />
     </main>
   );
 }
