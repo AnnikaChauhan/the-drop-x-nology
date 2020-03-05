d156338a54a33d8c66e4389fe9f7724987295555
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-05 15:43:25 +0000

    created new login page with a googlesignin button, due to changes with the databases.

--- src/components/Login/SignIn/SignIn.jsx
+++ src/components/Login/SignIn/SignIn.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import styles from "./SignIn.module.scss";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-// import LargeButton from '../../Utility/Button/LargeButton';
+import LargeButton from '../../Utility/Button/LargeButton';
 
 
 
@@ -14,30 +12,9 @@ class SignIn extends Component {
             <section className={styles.SignInContainer}>
                 <div className={styles.wrapper}>
                     <div className={styles.comet1}></div>
-                    <form className={styles.loginform}>
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Control className={styles.formcontain} type="email" placeholder="Enter email" />
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Control className={styles.formcontain} type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check className={styles.radiostyle} type="checkbox" label="Remember me?" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit" className={styles.loginBtn}>
-                                SIGN IN
-                            </Button>
-                        </Form>
-                    </form>
-                    <div className={styles.extra}>
-                        <div className={styles.link1}>Forgot Password</div>
-                        <div className={styles.link2}>New to The Drop?</div>
-                    </div>
-                    {/* <div className={styles.buttonStyle}>
+                    <div className={styles.buttonStyle}>
                         <LargeButton text={`Sign up with Google`} />
-                    </div> */}
+                    </div>
                 </div>
 
             </section>
