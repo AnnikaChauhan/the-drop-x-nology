e7ff7ca61418b930734f304bb34182616f8e3605
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-05 15:45:40 +0000

    attempting to fix the merge

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-import styles from "./Main.module.scss";
-import ReleaseArchive from "../../components/Main/ReleaseArchive";
-import FanDashboard from "../../containers/FanDashboard";
 
-function Main() {
-    return (
-        <section className={styles.Main}>
-            <FanDashboard />
-        </section>
-    );
-}
-
-export default Main;
