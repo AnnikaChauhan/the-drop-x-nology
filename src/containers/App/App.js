f01abe2d2e4fc450c975bbd0bcdfcc9f3b61dcf6
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-05 16:21:15 +0000

    finished the login page and now planning to push

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,6 +5,7 @@ import LandingPage from "../LandingPage";
 import Navbar from "../../components/Navbar";
 import Main from "../Main";
 
+
 function App() {
     return (
         <div className={styles.App}>
