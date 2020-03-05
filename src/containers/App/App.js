9fec08a402283c14c6162cbb4a81a4f162b60640
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-05 16:05:44 +0000

    Restructured ManageReleases, re-wrote to use Release Archive code with StatusBars. Moved StatusBar to Utility and altered CSS to fix flex.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,16 +4,11 @@ import styles from "./App.module.scss";
 import LandingPage from "../LandingPage";
 import Navbar from "../../components/Navbar";
 import Main from "../Main";
-import ChooseArtists from "../../components/ChooseArtists";
-import ReleaseArchive from "../../components/Main/ReleaseArchive";
-import ManageReleases from "../../components/Main/ReleaseArchive/ManageReleases/ManageReleases";
-import PreviewReleases from "../../components/Main/ReleaseArchive/ManageReleases/PreviewReleases";
 
 function App() {
     return (
         <div className={styles.App}>
             <Navbar />
-            <PreviewReleases />
             <Main />
         </div>
     );
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Main.module.scss";
 import ReleaseArchive from "../../components/Main/ReleaseArchive";
 import FanDashboard from "../../containers/FanDashboard";
+import ManageReleases from "../../components/Main/ManageReleases/ManageReleases";
+import PreviewReleases from "../../components/Main/ManageReleases/PreviewReleases";
 
 function Main() {
     return (
         <section className={styles.Main}>
             {/* Components to go in here */}
-            <FanDashboard />
+            <ReleaseArchive />
         </section>
     );
 }
