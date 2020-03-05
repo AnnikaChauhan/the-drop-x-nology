d1b317d241e272c3702e65f33d830be232151a84
Sophie Szathmary-Dixon <Sophie.Szathmary-Dixon@Nology.local>
2020-03-05 15:17:32 +0000

    comment out component from Main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -9,7 +9,7 @@ function Main() {
     return (
         <section className={styles.Main}>
             {/* Components to go in here */}
-            <ArtistBio />
+            {/* <ArtistBio /> */}
             {/* <FanDashboard /> */}
         </section>
     );
