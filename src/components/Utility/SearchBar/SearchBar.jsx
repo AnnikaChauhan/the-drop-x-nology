d6bb513d323492f082e2e549610f39034c368d38
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-10 16:45:19 +0000

    Removed commented SCSS code with DA and SA, deleted duplicate ManageRelease folder as part of tidy-up.

--- src/components/Utility/SearchBar/SearchBar.jsx
+++ src/components/Utility/SearchBar/SearchBar.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import styles from "./SearchBar.module.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 class SearchBar extends Component {
     render() {
         return (
-
             <div className={styles.searchBox}>
-                <input className={styles.searchInput} type="text" name="" placeholder="Search" />
+                <input
+                    className={styles.searchInput}
+                    type="text"
+                    name=""
+                    placeholder="Search"
+                />
                 <FontAwesomeIcon icon={faSearch} className={styles.icon} />
             </div>
+        );
+    }
+}
 
-                );
-            }
-        }
-        
-        export default SearchBar;
+export default SearchBar;
