1c4961db19806116689a164823df9956bc418ada
Dhiren Atodaria <Dhiren.Atodaria@Nology.local>
2020-03-10 15:40:02 +0000

    redid the styles for the search bar component, added a black backgorund as well with additional styling changed. No functionality has been added but in the future if the need arisis we can add in props for certain handlechange functions.

--- src/components/Utility/SearchBar/SearchBar.jsx
+++ src/components/Utility/SearchBar/SearchBar.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import styles from "./SearchBar.module.scss";
-import SmallButton from "../Buttons/SmallButton";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class SearchBar extends Component {
     render() {
         return (
-            <section className={styles.SearchBar}>
-                <input type="text" placeholder={this.props.placeHolder}></input><SmallButton text={"Search"} />
-            </section>
-        );
-    }
-}
 
-export default SearchBar;
+            <div className={styles.searchBox}>
+                <input className={styles.searchInput} type="text" name="" placeholder="Search" />
+                <FontAwesomeIcon icon={faSearch} className={styles.icon} />
+            </div>
+
+                );
+            }
+        }
+        
+        export default SearchBar;
