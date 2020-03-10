ce8810b05ddfe9d883b8e5b29b122408b8b864ff
Johnny Ji <Johnny.Ji@Nology.local>
2020-03-10 17:21:24 +0000

    replaced buttons with utility buttons and tick button added to utility

--- src/components/ChooseArtists/ChooseArtists.jsx
+++ src/components/ChooseArtists/ChooseArtists.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import styles from "./ChooseArtists.module.scss";
 import comet from "../../static/images/comet.png";
+import LargeButton from "../Utility/Buttons/LargeButton/LargeButton";
+import SmallButton from "../Utility/Buttons/SmallButton/SmallButton";
+import TickButton from "../Utility/Buttons/TickButton/TickButton";
 
 class ChooseArtists extends Component {
     render() {
@@ -8,19 +11,25 @@ class ChooseArtists extends Component {
             <section className={styles.ChooseArtists}>
                 <img className={styles.comet} src={comet} alt="The Drop Logo" />
                 <h2>We found these artists in your music library!</h2>
-                <button className={styles.followAll}>FOLLOW ALL</button>
+                <div className={styles.wrapperLargeButton}>
+                    <LargeButton className={styles.followAll} text = {"Follow ALL"}/>
+                </div>
                 <div className={styles.Artist}>
                     <div>Artist 1</div>
-                    <button>FOLLOW</button>
+                    <SmallButton text={"Follow"} />
                 </div>
                 <div className={`${styles.Artist} ${styles.RemoveBorder}`}>
                     <div>Artist 2</div>
-                    <button>FOLLOW</button>
+                    <SmallButton text={"Follow"} />
                 </div>
                 <div className={styles.Artist}>
                     <div>Artist 3</div>
-                    <button>FOLLOW</button>
+                    <TickButton />
                 </div>
+                <div className= {styles.wrapperLargeButton}>
+                    <LargeButton text = {"Sign Up"} />
+                </div>
+                
             </section>
         );
     }
