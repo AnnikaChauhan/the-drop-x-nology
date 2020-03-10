4ba118e4f707bf3e2280877f173b3c030ec5cf13
Stephen Arambulo <Stephen.Arambulo@Nology.local>
2020-03-10 17:10:28 +0000

    changed artist image positioning and added status bar based on wireframe

--- src/components/Main/Artist/Profile/Profile.jsx
+++ src/components/Main/Artist/Profile/Profile.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import styles from "./Profile.module.scss";
-import image from "../../../../static/images/sampleimage.jfif";
+// import image from "../../../../static/images/sampleimage.jfif";
+import image from "../../../../static/images/ei.jpg";
 import LargeButton from "../../../Utility/Buttons/LargeButton";
 import { Link } from "@reach/router";
+import StatusBar from "../../../Utility/StatusBar";
 
 export default class Profile extends Component {
     render() {
@@ -12,20 +14,33 @@ export default class Profile extends Component {
                     <div className={styles.imgWrapper}>
                         <img src={image} alt="Artist"></img>
                     </div>
+                    <div className={styles.editBio}>
+                        <p>Edit Bio</p>
+                    </div>
                     <div className={styles.artistWrapper}>
-                        <h2>Artist Name</h2>
+                        <h2>RICKY CHURCH</h2>
                     </div>
                 </div>
                 <div className={styles.textAndRelease}>
-                    <p>Dashboard > Artist Name</p>
                     <div>
-                        <Link to="add-release">
+                        {/* <Link to="add-release">
                             <LargeButton text={"Add Release"} />
-                        </Link>
+                        </Link> */}
+                    </div>
+                    <div className={styles.manageContain}>
+                        <p>Manage Releases</p>
+                    </div>
+                    <div className={styles.releaseContain}>
+                        <StatusBar title={"Release Title"} status={"released"} className={styles.boxx} />
+                        <StatusBar title={"Release Title"} status={"released"} className={styles.boxx} />
+                        <StatusBar title={"Release Title"} status={"released"} className={styles.boxx} />
+                        <StatusBar title={"Release Title"} status={"released"} className={styles.boxx} />
+                        <StatusBar title={"Release Title"} status={"released"} className={styles.boxx} />
+                        {/* <div> </ReleaseArchive /> </div>\ */}
                     </div>
                 </div>
                 <div className={styles.buttonsWrapper}>
-                    <Link to="releases">
+                    {/* <Link to="releases">
                         <LargeButton text={"Manage Releases"} />
                     </Link>
 
@@ -35,7 +50,7 @@ export default class Profile extends Component {
 
                     <Link to="archive">
                         <LargeButton text={"Release Archive"} />
-                    </Link>
+                    </Link> */}
                 </div>
             </section>
         );
