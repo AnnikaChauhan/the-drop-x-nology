ae669ce57f30732a141e30273b626e9d1a6b42a0
Annika Chauhan <Annika.Chauhan@Nology.local>
2020-03-11 16:19:36 +0000

    push up to github to include the firebase.js file with all sdk information added to be merged with development so that sunny and dhiren can work on the firebase database

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,11 +3,13 @@ import "./App.module.scss";
 import styles from "./App.module.scss";
 import Routes from "../../Routes/Routes";
 import ChooseArtists from "../../components/ChooseArtists/ChooseArtists";
+import LandingPage from "../LandingPage";
+import LoginPage from "../LoginPage";
 
 function App() {
     return (
         <div className={styles.App}>
-            <ChooseArtists />
+            <LoginPage />
             <Routes />
         </div>
     );
