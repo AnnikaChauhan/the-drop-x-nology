fd567403f1f1cd65f15088754cf45cc00fb552ed
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-11 22:13:22 +0000

    Removed superfluous sections from button utilities. Changed smallbutton class name to corresponding button, Btntext renamed text to match others.

--- src/components/Utility/Buttons/SmallButton/SmallButton.jsx
+++ src/components/Utility/Buttons/SmallButton/SmallButton.jsx
@@ -4,14 +4,12 @@ import styles from "./SmallButton.module.scss";
 class SmallButton extends Component {
     render() {
         return (
-            <section>
-                <div className={styles.smallbutton}>
-                    <div className={styles.translate}></div>
-                    <a href={this.props.link} className={styles.Btntext}>
-                        {this.props.text.toUpperCase()}
-                    </a>
-                </div>
-            </section>
+            <div className={styles.SmallButton}>
+                <div className={styles.translate}></div>
+                <a href={this.props.link} className={styles.text}>
+                    {this.props.text.toUpperCase()}
+                </a>
+            </div>
         );
     }
 }
