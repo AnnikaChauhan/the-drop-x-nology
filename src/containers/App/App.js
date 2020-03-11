ccbc3184bf2ef7010f9c388c61c4a7d84c4cb9e2
Johnny Ji <Johnny.Ji@Nology.local>
2020-03-11 15:03:38 +0000

    no changes made- only opened file

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.module.scss";
 import styles from "./App.module.scss";
 import Routes from "../../Routes/Routes";
+// import ReleaseCardList from "../Main/Fan/../"
 
 function App() {
     return (
         <div className={styles.App}>
+            <ReleaseCardList />
             <Routes />
         </div>
     );
