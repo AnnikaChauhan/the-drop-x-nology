1657582fc09bcfb87eb19f7deff4a6f922ce79ec
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-12 17:49:44 +0000

    Merged and fixed responsive problem with SearchList.

--- src/components/Main/Fan/Dashboard/ReleaseCardList/ReleaseCard/ReleaseCard.jsx
+++ src/components/Main/Fan/Dashboard/ReleaseCardList/ReleaseCard/ReleaseCard.jsx
@@ -4,17 +4,24 @@ import Card from "react-bootstrap/Card";
 
 class ReleaseCard extends Component {
     render() {
-        console.log(this.props.Releases)
         return (
             <Card style={{ border: "none" }} className={styles.cardWrapper}>
-                <Card.Img className={styles.image} variant="top" src={this.props.Releases.Artwork} />
+                <Card.Img
+                    className={styles.image}
+                    variant="top"
+                    src={this.props.Releases.Artwork}
+                />
                 <Card.Body className={styles.cardBody}>
-                    <Card.Title className={styles.artistName}>{this.props.Releases.Artist}</Card.Title>
+                    <Card.Title className={styles.artistName}>
+                        {this.props.Releases.Artist}
+                    </Card.Title>
                     <Card.Text className={styles.albumName}>
-                        {this.props.Releases.ReleaseName} <br/>
+                        {this.props.Releases.ReleaseName} <br />
                         Release Type: {this.props.Releases.ReleaseType}
-                        </Card.Text>
-                    <Card.Text className={styles.countDown}>{"12 Days"}</Card.Text>
+                    </Card.Text>
+                    <Card.Text className={styles.countDown}>
+                        {"12 Days"}
+                    </Card.Text>
                 </Card.Body>
             </Card>
         );
--- src/containers/Main/Fan/Dashboard/Dashboard.jsx
+++ src/containers/Main/Fan/Dashboard/Dashboard.jsx
@@ -13,57 +13,67 @@ class Dashboard extends Component {
         Releases: [],
         Artists: [],
         filteredArtists: []
-    }
+    };
 
     componentDidMount() {
         firestore
-          .collection("Releases")
-          .get()
-          .then(query => {
-            const Releases = query.docs.map(doc => doc.data());
-            this.setState({
-              Releases: Releases,
-              Artists: Releases
+            .collection("Releases")
+            .get()
+            .then(query => {
+                const Releases = query.docs.map(doc => doc.data());
+                this.setState({
+                    Releases: Releases,
+                    Artists: Releases
+                });
             });
-          });
     }
 
-    setSearchText = (event) => {
+    setSearchText = event => {
         const searchText = event.target.value;
-        this.setState({ searchText }, this.filteredArtists)
-    }
+        this.setState({ searchText }, this.filteredArtists);
+    };
 
     filteredArtists = () => {
         let filteredArtists = this.state.Artists.filter(artist => {
-            return artist.Artist.toUpperCase().includes(this.state.searchText.toUpperCase())
-        })
-        this.setState({ filteredArtists })
-    }
+            return artist.Artist.toUpperCase().includes(
+                this.state.searchText.toUpperCase()
+            );
+        });
+        this.setState({ filteredArtists });
+    };
 
     searchFocus = () => {
-        this.setState({searchFocused : true })
-        console.log("ok hello")
-    }
+        this.setState({ searchFocused: true });
+    };
 
     searchBlur = () => {
-        this.setState({searchFocused : false})
-        console.log("ok bye")
-    }
+        this.setState({ searchFocused: false });
+    };
 
     render() {
         if (this.state.searchFocused) {
             return (
                 <section className={styles.Dashboard}>
                     <Header title={"Search Results"} />
-                    <SearchBar onFocus={this.searchFocus} onBlur={this.searchBlur} searchText={this.state.searchText} onChange={this.setSearchText} placeHolder={"Search Artists..."} />
+                    <SearchBar
+                        onFocus={this.searchFocus}
+                        onBlur={this.searchBlur}
+                        searchText={this.state.searchText}
+                        onChange={this.setSearchText}
+                        placeHolder={"Search Artists..."}
+                    />
                     <SearchList Releases={this.state.filteredArtists} />
                 </section>
-            )
+            );
         } else {
             return (
                 <section className={styles.Dashboard}>
                     <Header title={"Dashboard"} />
-                    <SearchBar onFocus={this.searchFocus} onBlur={this.searchBlur} placeHolder={"Search Artists..."} />
+                    <SearchBar
+                        onFocus={this.searchFocus}
+                        onBlur={this.searchBlur}
+                        placeHolder={"Search Artists..."}
+                    />
                     <ReleaseCardList Releases={this.state.Releases} />
                 </section>
             );
