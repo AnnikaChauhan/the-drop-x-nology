5717ccb2b17e8a805c6a2e04df4ff6f5b1387fac
Sophie Szathmary-Dixon <Sophie.Szathmary-Dixon@Nology.local>
2020-03-12 17:35:51 +0000

    end of day

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.module.scss";
 import styles from "./App.module.scss";
 import Routes from "../../Routes/Routes";
+import Main from "../../components/";
+
 
 function App() {
     return (
         <div className={styles.App}>
-
+            <Main />
             <Routes />
         </div>
     );
