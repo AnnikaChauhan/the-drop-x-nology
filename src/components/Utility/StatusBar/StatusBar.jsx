9ed8b5959009d01c77331133451e6c10d0d84ec8
Andy Cameron <Andy.Cameron@Nology.local>
2020-03-15 22:06:55 +0000

    Added icons to StatusBar. Clicking on draft 'pencil' takes you to add-release without info.

--- src/components/Utility/StatusBar/StatusBar.jsx
+++ src/components/Utility/StatusBar/StatusBar.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from "react";
 import styles from "./StatusBar.module.scss";
+import { globalHistory } from "@reach/router";
 
 export default class StatusBar extends Component {
+    editRelease = () => {
+        globalHistory.navigate("dashboard/add-release");
+    };
+
+    get status() {
+        let classes = `${styles.status}`;
+        let content = ``;
+
+        switch (this.props.status) {
+            case "Live":
+                content = <i className={`fas fa-circle ${styles.live}`}></i>;
+                classes += ` ${styles.live}`;
+                break;
+            case "Draft":
+                content = (
+                    <i
+                        className={`fas fa-pencil-alt ${styles.edit}`}
+                        onClick={this.editRelease}
+                    ></i>
+                );
+                break;
+            default:
+                break;
+        }
+
+        return (
+            <div className={classes}>
+                {this.props.status}
+                {content}
+            </div>
+        );
+    }
+
     render() {
         return (
             <article className={styles.StatusBar}>
                 <div className={styles.title}>{this.props.title}</div>
                 <div className={styles.type}>{this.props.type}</div>
-                <div
-                    className={`${styles.status} ${
-                        this.props.status === "Live" ? `${styles.live}` : null
-                    }`}
-                >
-                    {this.props.status}
-                </div>
+                {this.status}
             </article>
         );
     }
